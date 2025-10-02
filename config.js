30e039a0dd50e9a5d81a9692277eb3454a1290a8
₱ⱤØ ₲₳₥ɆⱤ <senithaMC@gmail.com>
2025-10-02 19:18:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 module.exports = {
   prefix: ';',
-  token: 'MTQxNjQyMjk3ODEzNjMwOTc5Mw.GYJXlN.dbkjzjYm-w7GgSaQWQoopnYTEd1-7Vv9WPcQ5g',
-  mongoURI: 'mongodb+srv://darknessgmn_db_user:lsYafTHB1pHM9GEH@cluster1.gfsdsam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',
-  client_id: '1416422978136309793',
+  token: 'YOUR-BOT-TOKEN',
+  mongoURI: 'YOUR-MONDOFB-URI',
+  client_id: 'YOUR-BOT-ID',
   bot_name: 'LWKY',
-  ownerId: '1240540042926096406',
   commandCooldown: '10', // in seconds
   giveawayCleanup: {
     enabled: true,
