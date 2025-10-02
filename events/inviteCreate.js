3b846f3ca7f0599ab70315dbeb44c5cf59f40307
₱ⱤØ ₲₳₥ɆⱤ <senithaMC@gmail.com>
2025-10-02 19:08:04 +0530

    Update inviteCreate.js

--- events/inviteCreate.js
+++ events/inviteCreate.js
@@ -5,14 +5,12 @@ module.exports = {
   name: Events.InviteCreate,
   async execute(invite) {
     try {
-      // Check if the invite already exists
       let inviteData = await Invite.findOne({
         guildId: invite.guild.id,
         inviteCode: invite.code
       });
 
       if (!inviteData) {
-        // Create new invite record
         inviteData = new Invite({
           guildId: invite.guild.id,
           memberId: invite.inviter.id,
